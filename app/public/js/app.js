49846d8114b9af5d36e113c12ef394cbe9576f94
Matthew <majwang@ucsc.edu>
2016-07-03 11:14:03 -0700

    Added in login/logout and addmessage/deletemessage. Login and auth is unstable.

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,16 +1,48 @@
 var app =  angular.module('myApp',['firebase']);
-app.controller('myController', function($scope, $firebase){
-		
-		$scope.names = "Coder01";
-		
-		var ref = firebase.database();
-		$scope.messages = $firebase(ref);
-  
-		  $scope.addMessage = function(e) {
-				  if (e.keyCode != 13) return;
-				  $scope.messages.$add({from: $scope.name, body: $scope.msg});
-				  $scope.msg = "";
-		   }
-})
 
+app.controller("myController", function($scope, $firebaseArray) {
+	var ref = firebase.database().ref().child("messages");
+	  // create a synchronized array
+	  $scope.messages = $firebaseArray(ref);
+	  // add new items to the array
+	  // the message is automatically added to our Firebase database!
+	  $scope.addMessage = function() {
+		$scope.messages.$add({
+		  text: $scope.newMessageText
+		});
+	  };
 
+  }
+);
+
+app.controller("SampleCtrl", ["$scope", "$firebaseAuth",
+  function($scope, $firebaseAuth) {
+    var auth = $firebaseAuth();
+	var provider = new firebase.auth.GoogleAuthProvider();
+	$scope.signIn = function() {
+		firebase.auth().signInWithPopup(provider).then(function(result) {
+		  // This gives you a Google Access Token. You can use it to access the Google API.
+		  var token = result.credential.accessToken;
+		  // The signed-in user info.
+		  var user = result.user;
+		  // ...
+		}).catch(function(error) {
+		  // Handle Errors here.
+		  var errorCode = error.code;
+		  var errorMessage = error.message;
+		  // The email of the user's account used.
+		  var email = error.email;
+		  // The firebase.auth.AuthCredential type that was used.
+		  var credential = error.credential;
+		  // ...
+		});
+	}
+	$scope.signOut = function(){
+		firebase.auth().signOut().then(function() {
+		  // Sign-out successful.
+		}, function(error) {
+		  // An error happened.
+		});
+	}
+  }
+]);
