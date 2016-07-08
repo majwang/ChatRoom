98993ff001156342dd8f15a2d07f6cb5f9def609
Matthew <majwang@ucsc.edu>
2016-07-07 22:37:31 -0700

    Added login page.

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,31 +1,66 @@
-var app =  angular.module('myApp',['firebase']);
+var app =  angular.module('myApp',['firebase', 'ngRoute']);
 
-app.controller("myController", function($scope, $firebaseArray) {
-	var ref = firebase.database().ref().child("messages");
-	  // create a synchronized array
-	  $scope.messages = $firebaseArray(ref);
-	  // add new items to the array
-	  // the message is automatically added to our Firebase database!
-	  $scope.addMessage = function() {
-		$scope.messages.$add({
-		  text: $scope.newMessageText
-		});
-	  };
-
+app.factory("Auth", ["$firebaseAuth",
+  function($firebaseAuth) {
+    return $firebaseAuth();
   }
-);
+]);
+app.config(function($routeProvider) {
+	$routeProvider
 
-app.controller("SampleCtrl", ["$scope", "$firebaseAuth",
-  function($scope, $firebaseAuth) {
-    var auth = $firebaseAuth();
-	var provider = new firebase.auth.GoogleAuthProvider();
-	$scope.signIn = function() {
-		firebase.auth().signInWithPopup(provider).then(function(result) {
-		  // This gives you a Google Access Token. You can use it to access the Google API.
-		  var token = result.credential.accessToken;
+		// route for the home page
+		.when('/index', {
+			templateUrl : 'index.html',
+			controller  : 'chatController'
+		})
+
+		// route for the about page
+		.when('/login', {
+			templateUrl : 'login.html',
+			controller  : 'loginCtrl'
+		})
+
+});
+	
+app.controller("chatController", ["$scope", "$firebaseArray", "Auth", 
+	function($scope, $firebaseArray, Auth) {
+		$scope.auth = Auth;
+		$scope.auth.$onAuthStateChanged(function(firebaseUser) {
+			$scope.firebaseUser = firebaseUser;
+			
+		});
+		var ref = firebase.database().ref().child("messages");
+		  // create a synchronized array
+		  $scope.messages = $firebaseArray(ref);
+		  // add new items to the array
+		  // the message is automatically added to our Firebase database!
+		  $scope.addMessage = function() {
+			$scope.messages.$add({
+			  text: $scope.newMessageText
+			});
+		  };
+
+    }
+]);
+
+app.controller("loginCtrl", ["$scope", "Auth", "$window", "$location",
+  function($scope, Auth, $window, $location) {
+    $scope.auth = Auth;
+	$scope.auth.$onAuthStateChanged(function(firebaseUser) {
+        $scope.firebaseUser = firebaseUser;
+	    
+    });
+	$scope.signInG = function() {
+		var provider = new firebase.auth.GoogleAuthProvider();
+		firebase.auth().signInWithRedirect(provider);
+		firebase.auth().getRedirectResult().then(function(result) {
+		  if (result.credential) {
+			// This gives you a Google Access Token. You can use it to access the Google API.
+			var token = result.credential.accessToken;
+			// ...
+		  }
 		  // The signed-in user info.
 		  var user = result.user;
-		  // ...
 		}).catch(function(error) {
 		  // Handle Errors here.
 		  var errorCode = error.code;
@@ -34,14 +69,17 @@ app.controller("SampleCtrl", ["$scope", "$firebaseAuth",
 		  var email = error.email;
 		  // The firebase.auth.AuthCredential type that was used.
 		  var credential = error.credential;
-		  // ...
+		  console.log(errorMessage);
 		});
 	}
+	
 	$scope.signOut = function(){
 		firebase.auth().signOut().then(function() {
 		  // Sign-out successful.
+		  $window.location.reload();
 		}, function(error) {
 		  // An error happened.
+		  console.log(error.message);
 		});
 	}
   }
