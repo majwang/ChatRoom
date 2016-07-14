0d03d787e1087dbc04d8d1b1cee820387e38cea7
Matthew Wang <majwang@ucsc.edu>
2016-07-14 16:52:13 -0700

    Added in viewing other user's accounts through clicking on names in chat. Added profile pics.

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,4 +1,4 @@
-var app =  angular.module('myApp',['firebase', 'ngRoute', 'ngCart', 'ui.router' ]);
+var app =  angular.module('myApp',['firebase', 'ngRoute', 'ngCart', 'ui.router', "image-engine-angular" ]);
 var user;
 app.factory("Auth", ["$firebaseAuth",
   function($firebaseAuth) {
@@ -25,6 +25,7 @@ app.config(function($routeProvider, $stateProvider, $urlRouterProvider) {
 	$stateProvider
         .state('index', {
             url: '/index',
+            controller: 'indexCtrl',
 			views: {
 				"index": {
 					templateUrl: '/templates/home.html',
@@ -58,22 +59,29 @@ app.config(function($routeProvider, $stateProvider, $urlRouterProvider) {
 			controller: 'chatController'
         });
 });
-
+app.config(function (imgEngConfigProvider) {
+       imgEngConfigProvider.setToken('cyao');
+       imgEngConfigProvider.isLite(); // Add this line only for ImageEngine Lite
+    });
 app.controller("indexCtrl", ["$scope", "Auth", "$window", "$location", "$state",
 	function($scope, Auth, $window, $location, $state) {	
 		$scope.message = 'Home Page';
 		$scope.auth = Auth;
 		$scope.auth.$onAuthStateChanged(function(firebaseUser) {
         $scope.firebaseUser = firebaseUser;
-		console.log("Name: "+firebaseUser.displayName);
-		firebaseUser.providerData.forEach(function (profile) {
-		    console.log("Sign-in provider: "+profile.providerId);
-		    console.log("  Provider-specific UID: "+profile.uid);
-		    console.log("  Provider Name: "+profile.displayName);
-		    console.log("  Provider Email: "+profile.email);
-		    console.log("  Provider Photo URL: "+profile.photoURL);
-		  });
-	    user = firebaseUser;
+		if(firebaseUser){
+			console.log("UID: "+firebaseUser.uid);
+			/*irebaseUser.providerData.forEach(function (profile) {
+		    	console.log("Sign-in provider: "+profile.providerId);
+		    	console.log("  Provider-specific UID: "+profile.uid);
+		    	console.log("  Provider Name: "+profile.displayName);
+		    	console.log("  Provider Email: "+profile.email);
+		    	console.log("  Provider Photo URL: "+profile.photoURL);
+		  	});
+		  	*/
+			user = firebaseUser;
+		}
+	    
 		});
 	}
 ]);
@@ -87,22 +95,34 @@ app.controller("storeCtrl", ["$scope", "Auth", "$window", "$location", "ngCart",
 		$scope.auth = Auth;
 		$scope.auth.$onAuthStateChanged(function(firebaseUser) {
         $scope.firebaseUser = firebaseUser;
-		console.log("Name: "+firebaseUser.displayName);
-		firebaseUser.providerData.forEach(function (profile) {
-		    console.log("Sign-in provider: "+profile.providerId);
-		    console.log("  Provider-specific UID: "+profile.uid);
-		    console.log("  Provider Name: "+profile.displayName);
-		    console.log("  Provider Email: "+profile.email);
-		    console.log("  Provider Photo URL: "+profile.photoURL);
-		  });
-	    user = firebaseUser;
+			if(firebaseUser){
+			firebaseUser.providerData.forEach(function (profile) {
+		    	console.log("Sign-in provider: "+profile.providerId);
+		    	console.log("  Provider-specific UID: "+profile.uid);
+		    	console.log("  Provider Name: "+profile.displayName);
+		    	console.log("  Provider Email: "+profile.email);
+		    	console.log("  Provider Photo URL: "+profile.photoURL);
+		  	});
+			user = firebaseUser;
+			}
 		});
 	}
 ]);
-app.controller("accountCtrl", ["$scope", "Auth", "$window", "$state", "$stateParams", "myService",
-	function($scope, Auth, $window, $state, $stateParams, myService) {	
-		$scope.email = myService.get();
-		$scope.message = 'Account Page for: ' + $scope.email;
+app.controller("accountCtrl", ["$scope", "Auth", "$window", "$state", "$stateParams", "myService", "$sce",
+	function($scope, Auth, $window, $state, $stateParams, myService, $sce) {	
+		$scope.id = myService.get();
+		var a, b, c;
+		firebase.database().ref('/users/' + $scope.id).on('value', function(snapshot) { 
+  				updateInfo(snapshot.val());
+		});
+		function updateInfo(snapshot){
+			console.log(snapshot.username);
+  			$scope.name = snapshot.username;
+  			$scope.email = snapshot.email;
+  			$scope.message = 'Account Page for: ' + snapshot.username;
+  			$scope.pic = $sce.trustAsResourceUrl(snapshot.pic);
+  			$scope.$apply();
+		}
 	}
 ]);
 app.controller("forumsCtrl", ["$scope", "Auth", "$window", "$location",
@@ -116,18 +136,18 @@ app.controller("chatController", ["$scope", "$firebaseArray", "Auth", "$window",
 		$scope.auth = Auth;
 		$scope.auth.$onAuthStateChanged(function(firebaseUser) {
         $scope.firebaseUser = firebaseUser;
-		console.log("Name: "+firebaseUser.displayName + "," + firebaseUser.email);
-		firebaseUser.providerData.forEach(function (profile) {
-		    console.log("Sign-in provider: "+profile.providerId);
-		    console.log("  Provider-specific UID: "+profile.uid);
-		    console.log("  Provider Name: "+profile.displayName);
-		    console.log("  Provider Email: "+profile.email);
-		    console.log("  Provider Photo URL: "+profile.photoURL);
-			user = profile;
-		  });
-	    
+		if(firebaseUser){
+			/*firebaseUser.providerData.forEach(function (profile) {
+		    	console.log("Sign-in provider: "+profile.providerId);
+		    	console.log("  Provider-specific UID: "+profile.uid);
+		    	console.log("  Provider Name: "+profile.displayName);
+		    	console.log("  Provider Email: "+profile.email);
+		    	console.log("  Provider Photo URL: "+profile.photoURL);
+		  	});
+		  	*/
+			user = firebaseUser;
+			}
 		});
-		
 		var ref = firebase.database().ref().child("messages");
 		// create a synchronized array
 		$scope.messages = $firebaseArray(ref);
@@ -137,34 +157,47 @@ app.controller("chatController", ["$scope", "$firebaseArray", "Auth", "$window",
 			$scope.messages.$add({
 			  user: user.displayName,
 			  email: user.email,
+			  id: user.uid,
 			  text: $scope.newMessageText
 			});
 		};
-		$scope.goToAccount = function(email){
-			$scope.email = email;
-			myService.set($scope.email);
-			console.log("email: " + myService.get());
+		$scope.goToAccount = function(id){
+			$scope.id = id;
+			myService.set($scope.id);
+			console.log("id: " + myService.get());
 			$state.go('index.account');
 		}
     }
 ]);
 
-app.controller("loginCtrl", ["$scope", "Auth", "$window", "$location",
-  function($scope, Auth, $window, $location) {
+app.controller("loginCtrl", ["$scope", "Auth", "$window", "$location", "$sce",
+  function($scope, Auth, $window, $location, $sce) {
 	$scope.message = 'My Account';
 	
     $scope.auth = Auth;
 	$scope.auth.$onAuthStateChanged(function(firebaseUser) {
         $scope.firebaseUser = firebaseUser;
-		console.log("Name: "+firebaseUser.displayName);
-		firebaseUser.providerData.forEach(function (profile) {
-		    console.log("Sign-in provider: "+profile.providerId);
-		    console.log("  Provider-specific UID: "+profile.uid);
-		    console.log("  Provider Name: "+profile.displayName);
-		    console.log("  Provider Email: "+profile.email);
-		    console.log("  Provider Photo URL: "+profile.photoURL);
-		  });
-	    user = firebaseUser;
+		
+		if(firebaseUser){
+			firebaseUser.providerData.forEach(function (profile) {
+		    	//console.log("Sign-in provider: "+profile.providerId);
+		    	//console.log("  Provider-specific UID: "+profile.uid);
+		    	//console.log("  Provider Name: "+profile.displayName);
+		    	//console.log("  Provider Email: "+profile.email);
+		    	//console.log("  Provider Photo URL: "+profile.photoURL);
+		    	$scope.photoURL = $sce.trustAsResourceUrl(profile.photoURL);
+		  	});
+			user = firebaseUser;
+			
+			
+		    firebase.database().ref('users/' + firebaseUser.uid).set({
+    			username: firebaseUser.displayName,
+    			email: firebaseUser.email,
+    			id: firebaseUser.uid,
+    			pic: firebaseUser.photoURL
+  		  	});
+		}
+	    
     });
 	
 	$scope.rename = function(){
@@ -189,7 +222,8 @@ app.controller("loginCtrl", ["$scope", "Auth", "$window", "$location",
 			// ...
 		  }
 		  // The signed-in user info.
-		  var user = result.user;
+		  var firebaseUser = result.user;
+
 		}).catch(function(error) {
 		  // Handle Errors here.
 		  var errorCode = error.code;
